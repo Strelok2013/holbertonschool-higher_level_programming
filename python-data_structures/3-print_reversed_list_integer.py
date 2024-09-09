91ba71d8f04c11fe7abc29c7b13980b996931dba
Vlad <vgozhyi@gmail.com>
2024-09-09 08:46:19 +0000

    Fix pass 5

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-def print_reverrsed_list_integer(my_list=[]):
+def print_reversed_list_integer(my_list=[]):
     my_list.reverse()
     for i in my_list:
         print("{:d}".format(i))
