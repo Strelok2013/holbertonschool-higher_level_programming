4b03c2532b0c9b80b138b2c919a55459d680bad9
Vlad <vgozhyi@gmail.com>
2024-09-16 02:15:02 +0000

    Learning how to use isinstance and raise

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -9,21 +9,8 @@ def add_integer(a, b=98):
 
     Raises exception when not float or int???
     """
-    try:
-        if a or b is not int or float:
-            raise TypeError
-        if isinstance(a, float):
-            int_a = int(a)
-        else:
-            int_a = a
-        if isinstance(b, float):
-            int_b = int(b)
-        else:
-            int_b = b
-        return int_a + int_b
-         
-    except TypeError:
-        if a is not float or int:
-            print("a must be an integer")
-        if b is not float or int:
-            print("b must be an integer")
+    if((not isinstance(a, int) and not isinstance(a,float))):
+        raise TypeError("a must be an integer")
+    if((not isinstance(b, int) and not isinstance(b,float))):
+        raise TypeError("b must be an integer")
+    return(int(a) + int(b))
