1472996d4f57b411a37ef174f5947e197df9b891
Vlad <vgozhyi@gmail.com>
2024-09-17 01:11:32 +0000

    Fixed

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -2,15 +2,14 @@
 """Defines an integer addition function?"""
 
 def add_integer(a, b=98):
-    """
-    Returns the products of two integers or floats
+    """Returns the products of two integers or floats
 
     Casts floats to ints
 
     Raises exception when not float or int???
     """
-    if((not isinstance(a, int) and not isinstance(a,float))):
+    if ((not isinstance(a, int) and not isinstance(a, float))):
         raise TypeError("a must be an integer")
-    if((not isinstance(b, int) and not isinstance(b,float))):
+    if ((not isinstance(b, int) and not isinstance(b, float))):
         raise TypeError("b must be an integer")
     return(int(a) + int(b))
