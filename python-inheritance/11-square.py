d848b359c149c1ac125303f06725eb25b603ec94
Vlad <vgozhyi@gmail.com>
2024-09-24 07:19:27 +0000

    zaza

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Defines a module that defines the a subclass of Rectangle, Square"""
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
+Rectangle = __import__('9-rectangle.py').Rectangle
 
 
 class Square(Rectangle):
