688ca8fc5c51f74b14e2dd41b0ffe6567774b611
Vlad <vgozhyi@gmail.com>
2024-09-26 09:23:09 +0000

    Quick fix

--- python-abc/task_05_dragon.py
+++ python-abc/task_05_dragon.py
@@ -20,14 +20,8 @@ class Dragon(SwimMixin, FlyMixin):
     def roar(self):
         """"""
         print("The dragon roars!")
-    
-    def description(self):
-        """"""
-        print("An inferior specimen based on the perfect design,"
-              " workable, but not suited for the grand plan.")
-        
+     
 dragon = Dragon()
 dragon.swim()
 dragon.fly()
 dragon.roar()
-dragon.description()
