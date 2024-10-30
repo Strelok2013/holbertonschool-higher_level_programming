a50b7f9f59907afbad0c968406d58cd073efb660
Vlad <vgozhyi@gmail.com>
2024-10-30 20:18:44 +1100

    2 Fixes to SQL intro

--- SQL_introduction/6-list_values.sql
+++ SQL_introduction/6-list_values.sql
@@ -1,2 +1,2 @@
 -- Selects all rows from the table hbtn_0c_0
-SELECT * FROM hbtn_0c_0;
+SELECT * FROM first_table;
--- SQL_introduction/8-count_89.sql
+++ SQL_introduction/8-count_89.sql
@@ -1,2 +1,2 @@
 -- Shows the amount of rows in first_table
-SELECT COUNT(*) FROM first_table;
+SELECT COUNT(*) FROM first_table WHERE id = 89;
