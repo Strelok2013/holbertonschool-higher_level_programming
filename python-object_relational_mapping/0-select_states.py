4592e97f01d31f862e8edc4b62f6a6736b56f06c
Vlad <vgozhyi@gmail.com>
2024-11-04 13:39:04 +1100

    Wrong Keyword fix for task 1

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -9,16 +9,16 @@ if __name__ == "__main__":
     import MySQLdb
     import sys
 
-    host = "localhost"
-    port = 3306
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
+    h = "localhost"
+    p = 3306
+    usr = sys.argv[1]
+    pwd = sys.argv[2]
+    db = sys.argv[3]
 
-    db = MySQLdb.connect(host=host, port=port, username=username, password=password, database=database)
+    db = MySQLdb.connect(host=h, port=p, user=usr, password=pwd, database=db)
     cur = db.cursor()
 
-    cur.execute("SELECT * FROM %s", database)
+    cur.execute("SELECT * FROM states")
     rows = cur.fetchall()
     for row in rows:
         for col in row:
